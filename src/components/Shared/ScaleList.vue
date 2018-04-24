a01c4c1a31af6365d30c4f8b0b88287fcb22571b
mark <mark@iyb.ca>
2018-04-24 10:33:20 -0400

    first test

---
+++ src/components/Shared/ScaleList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+      name: 'ScaleList.vue'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Shared/YardList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+      name: 'YardList.vue'
+    }
+</script>
+
+<style scoped>
+
+</style>
