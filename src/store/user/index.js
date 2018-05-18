50b5b8f427b62ac5d1c16cf8598e17ea70bbddd1
mark <mark@iyb.ca>
2018-05-18 10:27:35 -0400

    new server next

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase'
+// import * as firebase from 'firebase'
 
 export default {
   state: {
@@ -7,19 +7,6 @@ export default {
     yards: []
   },
   mutations: {
-    registerUserForMeetup (state, payload) {
-      const id = payload.id
-      if (state.user.registeredMeetups.findIndex(meetup => meetup.id === id) >= 0) {
-        return
-      }
-      state.user.registeredMeetups.push(id)
-      state.user.fbKeys[id] = payload.fbKey
-    },
-    unregisterUserFromMeetup (state, payload) {
-      const registeredMeetups = state.user.registeredMeetups
-      registeredMeetups.splice(registeredMeetups.findIndex(meetup => meetup.id === payload), 1)
-      Reflect.deleteProperty(state.user.fbKeys, payload)
-    },
     setUser (state, payload) {
       state.user = payload
     },
@@ -31,61 +18,6 @@ export default {
     }
   },
   actions: {
-    registerUserForMeetup ({commit, getters}, payload) {
-      commit('setLoading', true)
-      const user = getters.user
-      firebase.database().ref('/users/' + user.id).child('/registrations/')
-        .push(payload)
-        .then(data => {
-          commit('setLoading', false)
-          commit('registerUserForMeetup', {id: payload, fbKey: data.key})
-        })
-        .catch(error => {
-          console.log(error)
-          commit('setLoading', false)
-        })
-    },
-    unregisterUserFromMeetup ({commit, getters}, payload) {
-      commit('setLoading', true)
-      const user = getters.user
-      if (!user.fbKeys) {
-        return
-      }
-      const fbKey = user.fbKeys[payload]
-      firebase.database().ref('/users/' + user.id + '/registrations/').child(fbKey)
-        .remove()
-        .then(() => {
-          commit('setLoading', false)
-          commit('unregisterUserFromMeetup', payload)
-        })
-        .catch(error => {
-          console.log(error)
-          commit('setLoading', false)
-        })
-    },
-    signUserUp ({commit}, payload) {
-      commit('setLoading', true)
-      commit('clearError')
-      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
-            commit('setLoading', false)
-            const newUser = {
-              id: user.uid,
-              registeredMeetups: [],
-              fbKeys: {}
-            }
-            commit('setUser', newUser)
-          }
-        )
-        .catch(
-          error => {
-            commit('setLoading', false)
-            commit('setError', error)
-            console.log(error)
-          }
-        )
-    },
     signUserIn ({commit, dispatch}, payload) {
       commit('setLoading', true)
       commit('clearError')
@@ -122,32 +54,8 @@ export default {
         fbKeys: {}
       })
     },
-    fetchUserData ({commit, getters}) {
-      commit('setLoading', true)
-      firebase.database().ref('/users/' + getters.user.id + '/registrations/').once('value')
-        .then(data => {
-          const dataPairs = data.val()
-          let registeredMeetups = []
-          let swappedPairs = {}
-          for (let key in dataPairs) {
-            registeredMeetups.push(dataPairs[key])
-            swappedPairs[dataPairs[key]] = key
-          }
-          const updatedUser = {
-            id: getters.user.id,
-            registeredMeetups: registeredMeetups,
-            fbKeys: swappedPairs
-          }
-          commit('setLoading', false)
-          commit('setUser', updatedUser)
-        })
-        .catch(error => {
-          console.log(error)
-          commit('setLoading', false)
-        })
-    },
     logout ({commit}) {
-      firebase.auth().signOut()
+      // firebase.auth().signOut()
       commit('setUser', null)
     },
     fetchYards ({commit, dispatch}) {
